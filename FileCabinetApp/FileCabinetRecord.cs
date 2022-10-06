bfd7f6b51c45853e070600777524025e3a3b01f4
Stanislav Zaycev <stas315172004@mail.ru>
2022-10-06 20:49:35 +0300

    Extend FileCabinetRecord class with properties - Age, IncomePerYear

--- FileCabinetApp/FileCabinetRecord.cs
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -10,10 +10,14 @@ public class FileCabinetRecord
 
     public string LastName { get; set; }
 
+    public short Age { get; set; }
+
     public DateTime DateOfBirth { get; set; }
+
+    public decimal IncomePerYear { get; set; }
 
     public override string ToString()
     {
-        return $"#{this.Id}, {this.FirstName}, {this.LastName}, {this.DateOfBirth.ToString("yyyy-MMM-dd", CultureInfo.CreateSpecificCulture("en-US"))}";
+        return $"#{this.Id}, {this.FirstName}, {this.LastName}, Age: {this.Age}, {this.DateOfBirth.ToString("yyyy-MMM-dd", CultureInfo.CreateSpecificCulture("en-US"))}, Income per year: {this.IncomePerYear:0.00}$";
     }
 }
--- FileCabinetApp/FileCabinetServise.cs
+++ FileCabinetApp/FileCabinetServise.cs
@@ -6,14 +6,16 @@ public class FileCabinetService
 {
     private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
 
-    public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+    public int CreateRecord(string firstName, string lastName, short age, DateTime dateOfBirth, decimal incomePerYear)
     {
         var record = new FileCabinetRecord
         {
             Id = this.list.Count + 1,
             FirstName = firstName,
             LastName = lastName,
+            Age = age,
             DateOfBirth = dateOfBirth,
+            AYearIncome = incomePerYear,
         };
 
         this.list.Add(record);
