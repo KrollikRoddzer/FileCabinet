9b84c6a9b3b128ccf6063ff299d80ebd584dba23
Stanislav Zaycev <stas315172004@mail.ru>
2022-10-06 12:15:53 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,12 @@
+namespace FileCabinetApp;
+
+public class FileCabinetRecord
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+}
